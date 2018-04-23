b2c3a5fa1be137e4cf5d2f648b147369ef3ae8a2
Anton McConville <anton@hickory.ca>
2018-04-22 22:05:57 -0400

    Adding standings

---
+++ secretmap/Standing.swift
@@ -0,0 +1,49 @@
+//
+//  Standing.swift
+//  fitcoin
+//
+//  Created by Anton McConville on 2018-04-22.
+//  Copyright © 2018 Anton McConville. All rights reserved.
+//
+
+import Foundation
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 11/10/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class Standing {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+}
